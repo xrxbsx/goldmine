f35022687830de12e0b605f88a6b080b6975bdd5
Armored Dragon <Armored-Dragon@users.noreply.github.com>
2016-11-09 16:25:54 -0800

    Fix updater

--- modules/admin.py
+++ modules/admin.py
@@ -37,17 +37,24 @@ class Admin:
     @commands.command()
     async def update(self):
         """Auto-updates this bot and restarts if any code was updated."""
-        await self.bot.say('Running `git pull`...\nOutput: ')
-        gitout = subprocess.check_output(['git', 'pull', '-v', '--ff'], stderr=subprocess.STDOUT)
-        await self.bot.say('```' + gitout + '```')
-        if gitout == 'Already up-to-date.\n':
-            await self.bot.say('Bot is already up-to-date, not restarting.')
+        await self.bot.say('Trying to update...')
+        try:
+            gitout = subprocess.check_output(['git', 'pull', '-v'], stderr=subprocess.STDOUT).decode('utf-8')
+        except subprocess.CalledProcessError as exp:
+            await self.bot.say('An error has occured while attempting to update!')
+            await self.bot.say('```' + str(exp) + '```')
+            await self.bot.say('Aborting. Contact Dragon5232 for help.')
+            gitout = False
+        if gitout != False:
+            await self.bot.say('Output:\n```' + gitout + '```')
+        if not gitout:
+            await self.bot.say('Update failed, not restarting.')
+        elif gitout.split('\n')[-2:][0] == 'Already up-to-date.':
+            await self.bot.say('Bot was already up-to-date, not restarting.')
         else:
             await self.bot.say('Bot was able to update, now restarting.')
-            await self.restart()
+            self.restart()
 
-    @commands.command()
-    async def restart(self):
+    def restart(self):
         """Restarts this bot."""
-        await self.bot.say('Goldmine is now restarting!')
         exit(0)
