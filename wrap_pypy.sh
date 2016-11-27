e5777450c397851160207b957a9cbfeae3a9b441
Armored Dragon <Armored-Dragon@users.noreply.github.com>
2016-11-27 09:25:45 -0800

    Fix wrapper executation

--- wrap_pypy.sh
+++ wrap_pypy.sh
@@ -2,15 +2,16 @@
 
 #cd "$(dirname \"$0\")"
 PYPY_PATH="~/pypy33/bin/pypy3"
+export PATH="$PYPY_PATH:$PATH"
 while true
 do
     sleep 5
     echo "Launching bot"
-    $PYPY_PATH goldmine.py
+    pypy3 goldmine.py
     echo "Clearing changes since last commit"
     git reset HEAD --hard
     echo "Updating code"
     git pull -v
     echo "Translating to Python 3.3.x asyncio syntax"
-    $PYPY_PATH convert-to-old-syntax.py
+    pypy3 convert-to-old-syntax.py
 done
