6ac66bc7395d8603566fef65d8d35a213c0923ac
Armored Dragon <Armored-Dragon@users.noreply.github.com>
2016-12-05 16:47:09 -0800

    Fix accidentally deleted file

---
+++ convert_to_old_syntax.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function
+from fnmatch import filter
+import os
+import re
+
+cur_dir = os.path.dirname(os.path.realpath(__file__))
+
+def rc_files(folder):
+    rl = []
+    for root, fds, files in os.walk(folder):
+        for fn in files:
+            rl.append(os.path.join(root, fn))
+    return rl
+
+def main():
+    targets = filter(rc_files(cur_dir), '*.py')
+    for target in targets:
+        with open(target, 'r+') as f:
+            contents = f.read()
+            f.seek(0)
+            f.truncate(0)
+            final = re.sub('await ', 'yield from ', contents)
+#            final = re.sub(r'(^\s*async def )', r'@asyncio.coroutine\n\1', final)
+            final = re.sub('async def ', '@asyncio.coroutine\ndef ', final)
+            final = re.sub('    @asyncio.coroutine\ndef ', '    @asyncio.coroutine\n    def ', final)
+            final = re.sub('async with ', 'with ', final)
+            final = re.sub('async for ', 'for ', final)
+#            print(final)
+#            input()
+            f.write(final)
+
+if __name__ == '__main__':
+    main()
