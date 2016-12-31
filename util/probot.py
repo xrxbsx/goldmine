9752e53d7279b83ff11edc5cb4ad0b0d3c9ca8fc
Armored Dragon <Armored-Dragon@users.noreply.github.com>
2016-12-31 00:15:14 -0500

    Error handling finished for now

--- util/probot.py
+++ util/probot.py
@@ -478,7 +478,7 @@ Remember to use the custom emotes{2} for extra fun! You can access my help with 
 
     async def on_error(self, ev_name, *ev_args, **ev_kwargs):
         kw_args = ', ' + (', '.join([k + '=' + str(ev_kwargs[k]) for k in ev_kwargs])) if ev_kwargs else ''
-        self.logger.error(f'Event handler {ev_name} errored! Called with ' + ', '.join([str(i) for i in ev_args]) + kw_args)
+        self.logger.error(f'Event handler {ev_name} errored! Called with ' + ', '.join([bdel(str(i), 'Command raised an exception: ') for i in ev_args]) + kw_args)
 
     async def on_server_join(self, server):
         """Send the bot introduction message when invited."""
