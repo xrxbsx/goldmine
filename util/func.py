4d6ae9d61a7d40fcf57f0baa051c7d5184596764
Armored Dragon <Armored-Dragon@users.noreply.github.com>
2017-01-01 00:54:50 -0500

    Polish encoding and decoding

--- util/func.py
+++ util/func.py
@@ -66,7 +66,7 @@ def encode(content: str) -> str:
     shift = round(random.uniform(1, 145), random.randint(3, 6))
     shift_shift = random.randint(1, 14)
     shifted_ords = [float(o) + shift for o in orig_ords]
-    join_chars = list('@!($)_*#%"}?\'=-`plk\\][º¢§≤æ•')
+    join_chars = list('@!($)_*#%"}?\'=-`plk\\][')
     join_char = random.choice(join_chars)
     ords_str = join_char.join([str(s) for s in shifted_ords]) + '~' + join_char.join([str(float(ord(c)) + shift) for c in list('3MainShiftCorrect')])
     fn_head_join = random.choice(list('|^&'))
@@ -81,37 +81,40 @@ def encode(content: str) -> str:
     return final
 
 def decode(content: str) -> str:
-    expected_key = '3MainShiftCorrect'
-    join_chars = list('@!($)_*#%"}?\'=-`plk\\][º¢§≤æ•')
-    shift_key = content.split('~')[1]
-    content = content.replace('~' + shift_key, '') # discard shift key
-    head_keys = {}
-    for try_decode_head in list('|^&'):
-        if try_decode_head in content:
-            dec_head_1 = content.split(try_decode_head)[0]
-            head_r_keys = dec_head_1.split(';')
-            for rkey in head_r_keys:
-                head_keys[rkey[0]] = rkey[1:]
-            no_head_content = content.replace(dec_head_1 + try_decode_head, '')
-    head_keys['d'] = float(head_keys['d'])
-    head_keys['g'] = float(head_keys['g'])
-    head_keys['l'] = float(head_keys['l'])
-    j = join_chars[int((head_keys['l'] + 4.4689257) - head_keys['g'])]
-    for try_shift_shift in range(1, 14):
-        shift_to_try = head_keys['d'] - float(try_shift_shift)
-        if ''.join([chr(int(cn - shift_to_try)) for cn in [float(sf) for sf in shift_key.split(j)]]) == expected_key:
-            shift = shift_to_try
-            break
-    content = no_head_content
-    dec = ''.join([chr(int(cn - shift)) for cn in [float(sf) for sf in content.split(j)]])
-    return dec
+    try:
+        expected_key = '3MainShiftCorrect'
+        join_chars = list('@!($)_*#%"}?\'=-`plk\\][')
+        shift_key = content.split('~')[1]
+        content = content.replace('~' + shift_key, '') # discard shift key
+        head_keys = {}
+        for try_decode_head in list('|^&'):
+            if try_decode_head in content:
+                dec_head_1 = content.split(try_decode_head)[0]
+                head_r_keys = dec_head_1.split(';')
+                for rkey in head_r_keys:
+                    head_keys[rkey[0]] = rkey[1:]
+                no_head_content = content.replace(dec_head_1 + try_decode_head, '')
+        head_keys['d'] = float(head_keys['d'])
+        head_keys['g'] = float(head_keys['g'])
+        head_keys['l'] = float(head_keys['l'])
+        j = join_chars[int((head_keys['l'] + 4.4689257) - head_keys['g'])]
+        for try_shift_shift in range(1, 14):
+            shift_to_try = head_keys['d'] - float(try_shift_shift)
+            if ''.join([chr(int(cn - shift_to_try)) for cn in [float(sf) for sf in shift_key.split(j)]]) == expected_key:
+                shift = shift_to_try
+                break
+        content = no_head_content
+        dec = ''.join([chr(int(cn - shift)) for cn in [float(sf) for sf in content.split(j)]])
+        return dec
+    except Exception:
+        return '⚠ Couldn\'t decode. Maybe your content is corrupted?'
 
 async def async_encode(content: str) -> str:
     orig_ords = [ord(c) for c in list(content)]
     shift = round(random.uniform(1, 145), random.randint(3, 6))
     shift_shift = random.randint(1, 14)
     shifted_ords = [float(o) + shift for o in orig_ords]
-    join_chars = list('@!($)_*#%"}?\'=-`plk\\][º¢§≤æ•')
+    join_chars = list('@!($)_*#%"}?\'=-`plk\\][')
     join_char = random.choice(join_chars)
     ords_str = join_char.join([str(s) for s in shifted_ords]) + '~' + join_char.join([str(float(ord(c)) + shift) for c in list('3MainShiftCorrect')])
     fn_head_join = random.choice(list('|^&'))
@@ -126,27 +129,30 @@ async def async_encode(content: str) -> str:
     return final
 
 async def async_decode(content: str) -> str:
-    expected_key = '3MainShiftCorrect'
-    join_chars = list('@!($)_*#%"}?\'=-`plk\\][º¢§≤æ•')
-    shift_key = content.split('~')[1]
-    content = content.replace('~' + shift_key, '') # discard shift key
-    head_keys = {}
-    for try_decode_head in list('|^&'):
-        if try_decode_head in content:
-            dec_head_1 = content.split(try_decode_head)[0]
-            head_r_keys = dec_head_1.split(';')
-            for rkey in head_r_keys:
-                head_keys[rkey[0]] = rkey[1:]
-            no_head_content = content.replace(dec_head_1 + try_decode_head, '')
-    head_keys['d'] = float(head_keys['d'])
-    head_keys['g'] = float(head_keys['g'])
-    head_keys['l'] = float(head_keys['l'])
-    j = join_chars[int((head_keys['l'] + 4.4689257) - head_keys['g'])]
-    for try_shift_shift in range(1, 14):
-        shift_to_try = head_keys['d'] - float(try_shift_shift)
-        if ''.join([chr(int(cn - shift_to_try)) for cn in [float(sf) for sf in shift_key.split(j)]]) == expected_key:
-            shift = shift_to_try
-            break
-    content = no_head_content
-    dec = ''.join([chr(int(cn - shift)) for cn in [float(sf) for sf in content.split(j)]])
-    return dec
+    try:
+        expected_key = '3MainShiftCorrect'
+        join_chars = list('@!($)_*#%"}?\'=-`plk\\][')
+        shift_key = content.split('~')[1]
+        content = content.replace('~' + shift_key, '') # discard shift key
+        head_keys = {}
+        for try_decode_head in list('|^&'):
+            if try_decode_head in content:
+                dec_head_1 = content.split(try_decode_head)[0]
+                head_r_keys = dec_head_1.split(';')
+                for rkey in head_r_keys:
+                    head_keys[rkey[0]] = rkey[1:]
+                no_head_content = content.replace(dec_head_1 + try_decode_head, '')
+        head_keys['d'] = float(head_keys['d'])
+        head_keys['g'] = float(head_keys['g'])
+        head_keys['l'] = float(head_keys['l'])
+        j = join_chars[int((head_keys['l'] + 4.4689257) - head_keys['g'])]
+        for try_shift_shift in range(1, 14):
+            shift_to_try = head_keys['d'] - float(try_shift_shift)
+            if ''.join([chr(int(cn - shift_to_try)) for cn in [float(sf) for sf in shift_key.split(j)]]) == expected_key:
+                shift = shift_to_try
+                break
+        content = no_head_content
+        dec = ''.join([chr(int(cn - shift)) for cn in [float(sf) for sf in content.split(j)]])
+        return dec
+    except Exception:
+        return '⚠ Couldn\'t decode. Maybe your content is corrupted?'
